fd45b02a395b3f5a167dd3f9ac6de6cca5bc976f
david <kryptn@gmail.com>
2011-12-06 02:27:11 -0600

    added storage class, handles the ~physical~ locations of the boxes.

--- packing.py
+++ packing.py
@@ -18,9 +18,51 @@ class Box(object):
    def box(self):
       """ returns appropriate tuple. """
       return (self.width, self.height)
+   
+   @property
+   def coord(self):
+      return (self.x, self.y)
 
    def __repr__(self):
       return str(self.box)
+
+class Storage(object):
+
+   def validate(self, x, y):
+      """ checks current box locations """
+      valid = False
+      for i, box in enumerate(self.boxes):
+         cond1 = box.x <= x < box.x+box.width
+         cond2 = box.y <= y < box.y+box.height
+         if cond1 and cond2:
+            return i
+      return valid
+
+   def place(self, box, x, y):
+      box.x = x
+      box.y = y
+      self.boxes.append(box)
+      if self.width < box.x + box.width:
+         self.width = box.x + box.width
+      if self.height < box.y + box.height:
+         self.height = box.y + box.height
+   
+   def display(self):
+      """ builds 2d list with boxes """
+      for x in xrange(self.height):
+         for y in xrange(self.width):
+            g = self.validate(y, x)
+            if g:
+               print g,
+            else:
+               print 'x',
+         print
+
+
+   def __init__(self):
+      self.width = 0
+      self.height = 0
+      self.boxes = list()
 
 class Pack(object):
    def load(self, sample):
@@ -67,7 +109,7 @@ class Pack(object):
 
       while boxes:
          box = boxes.pop(self.largest(boxes))
-         
+
 
       
    def __init__(self, sample=None):
@@ -83,5 +125,7 @@ if __name__ == '__main__':
    sample = [(1, 3), (2, 5), (6, 1), (2, 7), (8, 2), (9, 7), (3, 5)]
    a = Pack(sample)
    #a = Pack()
+   s = Storage()
+   s.place(Box(2,3),3,4)
    print 'boxes:', a.boxes
    print 'optimal:', a.optimal
